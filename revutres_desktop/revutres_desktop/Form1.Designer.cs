29843a61f2aae4dc6cb3ff3425948b6c9d1a8579
micaelomota <micaelomota@gmail.com>
2016-03-06 21:41:45 -0300

    iniciando e fazendo comunicação serial corretamente

---
+++ revutres_desktop/revutres_desktop/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace revutres_desktop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btn_conectar = new System.Windows.Forms.Button();
+            this.comboBoxPortasCOM = new System.Windows.Forms.ComboBox();
+            this.btn_enviar = new System.Windows.Forms.Button();
+            this.textBoxEnviar = new System.Windows.Forms.TextBox();
+            this.textBoxReceber = new System.Windows.Forms.TextBox();
+            this.timerCOM = new System.Windows.Forms.Timer(this.components);
+            this.serialPortCOM = new System.IO.Ports.SerialPort(this.components);
+            this.SuspendLayout();
+            // 
+            // btn_conectar
+            // 
+            this.btn_conectar.Location = new System.Drawing.Point(39, 39);
+            this.btn_conectar.Name = "btn_conectar";
+            this.btn_conectar.Size = new System.Drawing.Size(75, 23);
+            this.btn_conectar.TabIndex = 0;
+            this.btn_conectar.Text = "Conectar";
+            this.btn_conectar.UseVisualStyleBackColor = true;
+            this.btn_conectar.Click += new System.EventHandler(this.btn_conectar_Click);
+            // 
+            // comboBoxPortasCOM
+            // 
+            this.comboBoxPortasCOM.FormattingEnabled = true;
+            this.comboBoxPortasCOM.Location = new System.Drawing.Point(157, 40);
+            this.comboBoxPortasCOM.Name = "comboBoxPortasCOM";
+            this.comboBoxPortasCOM.Size = new System.Drawing.Size(129, 21);
+            this.comboBoxPortasCOM.TabIndex = 1;
+            // 
+            // btn_enviar
+            // 
+            this.btn_enviar.Location = new System.Drawing.Point(39, 69);
+            this.btn_enviar.Name = "btn_enviar";
+            this.btn_enviar.Size = new System.Drawing.Size(75, 23);
+            this.btn_enviar.TabIndex = 2;
+            this.btn_enviar.Text = "Enviar";
+            this.btn_enviar.UseVisualStyleBackColor = true;
+            this.btn_enviar.Click += new System.EventHandler(this.btn_enviar_Click);
+            // 
+            // textBoxEnviar
+            // 
+            this.textBoxEnviar.Location = new System.Drawing.Point(157, 71);
+            this.textBoxEnviar.Name = "textBoxEnviar";
+            this.textBoxEnviar.Size = new System.Drawing.Size(474, 20);
+            this.textBoxEnviar.TabIndex = 3;
+            // 
+            // textBoxReceber
+            // 
+            this.textBoxReceber.Location = new System.Drawing.Point(39, 114);
+            this.textBoxReceber.Multiline = true;
+            this.textBoxReceber.Name = "textBoxReceber";
+            this.textBoxReceber.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxReceber.Size = new System.Drawing.Size(592, 194);
+            this.textBoxReceber.TabIndex = 4;
+            // 
+            // timerCOM
+            // 
+            this.timerCOM.Interval = 1000;
+            this.timerCOM.Tick += new System.EventHandler(this.timerCOM_Tick);
+            // 
+            // serialPortCOM
+            // 
+            this.serialPortCOM.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPortCOM_DataReceived);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(669, 334);
+            this.Controls.Add(this.textBoxReceber);
+            this.Controls.Add(this.textBoxEnviar);
+            this.Controls.Add(this.btn_enviar);
+            this.Controls.Add(this.comboBoxPortasCOM);
+            this.Controls.Add(this.btn_conectar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_conectar;
+        private System.Windows.Forms.ComboBox comboBoxPortasCOM;
+        private System.Windows.Forms.Button btn_enviar;
+        private System.Windows.Forms.TextBox textBoxEnviar;
+        private System.Windows.Forms.TextBox textBoxReceber;
+        private System.Windows.Forms.Timer timerCOM;
+        private System.IO.Ports.SerialPort serialPortCOM;
+    }
+}
+
---
+++ revutres_desktop/revutres_desktop/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;  // necessário para ter acesso as portas
+
+namespace revutres_desktop
+{
+    public partial class Form1 : Form
+    {
+        string resposta_arduino;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Console.Write("TESTE");
+            timerCOM.Enabled = true;
+        }
+
+        public void refreshCOMList()
+        {
+            int i;
+            bool quantDiferente;    //flag para sinalizar que a quantidade de portas mudou
+
+            i = 0;
+            quantDiferente = false;
+
+            //se a quantidade de portas mudou
+            if (comboBoxPortasCOM.Items.Count == SerialPort.GetPortNames().Length)
+            {
+                foreach (string s in SerialPort.GetPortNames())
+                {
+                    if (comboBoxPortasCOM.Items[i++].Equals(s) == false)
+                    {
+                        quantDiferente = true;
+                    }
+                }
+            }
+            else
+            {
+                quantDiferente = true;
+            }
+
+            //Se não foi detectado diferença
+            if (quantDiferente == false)
+            {
+                return;                     //retorna
+            }
+
+            //limpa comboBox
+            comboBoxPortasCOM.Items.Clear();
+
+            //adiciona todas as COM diponíveis na lista
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                comboBoxPortasCOM.Items.Add(s);
+            }
+            //seleciona a primeira posição da lista
+            comboBoxPortasCOM.SelectedIndex = 0;
+        }
+
+        private void timerCOM_Tick(object sender, EventArgs e)
+        {
+            refreshCOMList(); // Atualizando a lista de portas  a cada 1 segundo
+        }
+
+        private void btn_conectar_Click(object sender, EventArgs e)
+        {
+            if (serialPortCOM.IsOpen == false)
+            {
+                try
+                {
+                    serialPortCOM.PortName = comboBoxPortasCOM.Items[comboBoxPortasCOM.SelectedIndex].ToString();
+                    serialPortCOM.Open();
+
+                }
+                catch
+                {
+                    return;
+                }
+                if (serialPortCOM.IsOpen)
+                {
+                    btn_conectar.Text = "Desconectar";
+                    comboBoxPortasCOM.Enabled = false;
+
+                }
+            }
+            else
+            {
+
+                try
+                {
+                    serialPortCOM.Close();
+                    comboBoxPortasCOM.Enabled = true;
+                    btn_conectar.Text = "Conectar";
+                }
+                catch
+                {
+                    return;
+                }
+
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (serialPortCOM.IsOpen == true)  // se porta aberta 
+                serialPortCOM.Close();            //fecha a porta
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_enviar_Click(object sender, EventArgs e)
+        {
+            if (serialPortCOM.IsOpen == true)          //porta está aberta
+                serialPortCOM.Write(textBoxEnviar.Text);  //envia o texto presente no textbox Enviar
+        }
+
+        private void serialPortCOM_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            resposta_arduino = serialPortCOM.ReadExisting();              //le o dado disponível na serial
+            this.Invoke(new EventHandler(trataDadoRecebido));   //chama outra thread para escrever o dado no text box
+        }
+
+        private void trataDadoRecebido(object sender, EventArgs e)
+        {
+            Console.Write("TESTE recebido");
+            textBoxReceber.AppendText(resposta_arduino);
+        }
+    }
+}
